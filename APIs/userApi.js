7e257760bedaceaaa26efc4350f6f6bdeba572c0
rajesh <rajesh.t7982@gmail.com>
2024-11-12 20:21:41 +0530

    user api with db interaction

--- APIs/userApi.js
+++ APIs/userApi.js
@@ -1,79 +1,104 @@
 //create mini-exp app(Special route)
-const exp=require('express')
-const userApp=exp.Router()
+const exp = require("express");
+const userApp = exp.Router();
 
 //use body parser middeleware
-userApp.use(exp.json())
+userApp.use(exp.json());
 
-//sample users list(Replaced with DB in future)
-let usersList = [
-    { id: 100, name: "ravi", age: 31 },
-    { id: 200, name: "manasa", age: 21 },
-  ];
-  
-  //Create REST API(Routes)
-  //A route is used to handle client req
-  //route to handle get req
-  userApp.get("/users",(req, res) => {
-    res.send({ message: "users", payload: usersList });
-  });
-  
-  //route to to handle get req to get a user by id
-  userApp.get("/users/:id", (req, res) => {
-    //read value of url parameter
-    let userId = Number(req.params.id); //{ id : 10}
-    //find user with this userId
-    let user = usersList.find((userObj) => userObj.id === userId);
-    if (user === undefined) {
-      res.send({ message: "No user found" });
-    } else {
-      //send res
-      res.send({ message: "A user", payload: user });
-    }
-  });
-  
-  //route to handle post req(create new user)
-  userApp.post("/user",(req, res) => {
-    //read data send by the client in port req
-    let newUser = req.body;
-    //add new user to usersList
-    usersList.push(newUser);
-    //send res
-    res.send({ message: "New user added" });
-  });
-  
-  //route to handle put req(update user)
-  userApp.put("/user", (req, res) => {
-    //get modifiedUSer from client
-    let modifiedUSer = req.body;
-    //get index of user with id of modifiedUser
-    let userIndex = usersList.findIndex(
-      (userObj) => userObj.id === modifiedUSer.id
-    );
-    //if user not found
-    if (userIndex === -1) {
-      res.send({ message: "User not found to update" });
-    } else {
-      usersList.splice(userIndex, 1, modifiedUSer);
-      res.send({ message: "User modified" });
-    }
-  });
-  
-  //route to delete user
-  userApp.delete("/users/:id", (req, res) => {
-    //read value of url parameter
-    let userId = Number(req.params.id);
-    //get index of user with id of modifiedUser
-    let userIndex = usersList.findIndex((userObj) => userObj.id === userId);
-    //if user not found
-    if (userIndex === -1) {
-      res.send({ message: "User not found to delete" });
-    } else {
-      usersList.splice(userIndex, 1);
-      res.send({ message: "User deleted" });
-    }
-  });
+//Create REST API(Routes)
+//A route is used to handle client req
+//route to handle get req
+userApp.get("/users",async (req, res) => {
+  try{
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //read users from db
+  let usersList= await usersCollectionObj.find().toArray()
+  //send res
+  res.send({message:"all users",payload:usersList})
+  }catch(err){
+    res.send({message:"error occurred",error:err.message})
+  }
+});
 
 
-  //export userApp
-  module.exports=userApp;
+
+
+
+//route to to handle get req to get a user by name
+userApp.get("/users/:name", async(req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get name from url
+  let nameOfUrl=req.params.name;
+  //get user by name
+  let user=await usersCollectionObj.findOne({name:nameOfUrl})
+  console.log(user)
+  //if user  not found
+  if(user===null){
+    res.send({message:"No user found"})
+  }else{
+    res.send({message:"user found",payload:user})
+  }
+
+});
+
+
+
+
+
+//route to handle post req(create new user)
+userApp.post("/user", async(req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get body of req
+  let userFromClient=req.body;
+  //insert
+  let dbRes= await usersCollectionObj.insertOne(userFromClient)
+ // console.log(dbRes)
+  //send res
+  res.send({message:"New user created"})
+});
+
+
+
+
+
+
+
+
+//route to handle put req(update user)
+userApp.put("/user", async(req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get modified user
+  let modifiedUser=req.body;
+  //update
+  let dbRes=await usersCollectionObj.updateOne({name:modifiedUser.name},{$set:{...modifiedUser}})
+
+  if(dbRes.modifiedCount===1){
+    res.send({message:"User modified"})
+  }else{
+    res.send({message:"No modification"})
+  }
+});
+
+
+
+
+
+//route to delete user
+userApp.delete("/users/:name", async(req, res) => {
+  //get usersCollectionObj
+  const usersCollectionObj = req.app.get("usersCollectionObj");
+  //get name from url
+  let nameOfUrl=req.params.name;
+  //delete
+  let dbRes=await usersCollectionObj.deleteOne({name:nameOfUrl})
+  //send res
+  res.send({message:"User removed"})
+
+});
+
+//export userApp
+module.exports = userApp;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const exp = require("express");
 const app = exp();
-app.listen(3000, () => console.log("http server listening on port 3000"));
+
 
 
 //Connect to MongoDB server
@@ -10,8 +10,15 @@ app.listen(3000, () => console.log("http server listening on port 3000"));
    const mClient= require('mongodb').MongoClient
     //connect to DB 
     mClient.connect('mongodb://localhost:27017/vnrb2')
-    .then(()=>{
+    .then((client)=>{
+        //get db obj
+        const dbObj=client.db('vnrb2')
+        //get collection object
+        const usersCollectionObj=dbObj.collection('users')
+        //share usersCollectionObj
+        app.set('usersCollectionObj',usersCollectionObj)
         console.log("DB connection is success")
+        app.listen(3000, () => console.log("http server listening on port 3000"));
     })
     .catch(err=>{
         console.log("Error in DB connection ",err)
@@ -25,3 +32,9 @@ app.use('/user-api',userApi)
 
 //forward req to productApi , if path starts with /product-api
 app.use('/product-api',productApi)
+
+
+
+
+
+
